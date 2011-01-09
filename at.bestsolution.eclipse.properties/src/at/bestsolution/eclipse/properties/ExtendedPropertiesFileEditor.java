8b1ffa06ff3839ca7dd216e4666e89108085c39b
tomsontom <tomsontom@737c282a-10f1-5f32-d127-9b87a3a82760>
2011-01-09 15:32:53 +0000

    * move document listening to adapter

--- at.bestsolution.eclipse.properties/src/at/bestsolution/eclipse/properties/ExtendedPropertiesFileEditor.java
+++ at.bestsolution.eclipse.properties/src/at/bestsolution/eclipse/properties/ExtendedPropertiesFileEditor.java
@@ -10,24 +10,15 @@
  ******************************************************************************/
 package at.bestsolution.eclipse.properties;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.Map.Entry;
-import java.util.Properties;
-
 import org.eclipse.jdt.internal.ui.propertiesfileeditor.PropertiesFileEditor;
 import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IDocumentListener;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
 
-import at.bestsolution.eclipse.properties.PropertyContentOutlinePage.Pair;
 import at.bestsolution.eclipse.properties.PropertyContentOutlinePage.Property;
 import at.bestsolution.eclipse.properties.PropertyContentOutlinePage.PropertyGroup;
 
@@ -36,38 +27,7 @@ public class ExtendedPropertiesFileEditor extends PropertiesFileEditor {
 	@Override
 	public Object getAdapter(@SuppressWarnings("rawtypes") Class adapter) {
 		if (adapter == IContentOutlinePage.class) {
-			final PropertyContentOutlinePage contentOutline = new PropertyContentOutlinePage();
-
-			getDocumentProvider().getDocument(getEditorInput())
-					.addDocumentListener(new IDocumentListener() {
-
-						public void documentChanged(DocumentEvent event) {
-							try {
-								contentOutline.setProperties(getPairs());
-							} catch (UnsupportedEncodingException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}
-
-						public void documentAboutToBeChanged(DocumentEvent event) {
-							// TODO Auto-generated method stub
-
-						}
-					});
-
-			try {
-				contentOutline.setProperties(getPairs());
-			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			final PropertyContentOutlinePage contentOutline = new PropertyContentOutlinePage(this);
 
 			contentOutline
 					.addSelectionChangedListener(new ISelectionChangedListener() {
@@ -117,17 +77,5 @@ public class ExtendedPropertiesFileEditor extends PropertiesFileEditor {
 		//
 		// TODO Auto-generated method stub
 		return super.getAdapter(adapter);
-	}
-
-	public Pair[] getPairs() throws UnsupportedEncodingException, IOException {
-		Properties p = new Properties();
-		p.load(new ByteArrayInputStream(getDocumentProvider()
-				.getDocument(getEditorInput()).get().getBytes("UTF-8")));
-		Pair[] pairs = new Pair[p.entrySet().size()];
-		int i = 0;
-		for (Entry<Object, Object> e : p.entrySet()) {
-			pairs[i++] = new Pair((String) e.getKey(), (String) e.getValue());
-		}
-		return pairs;
 	}
 }
