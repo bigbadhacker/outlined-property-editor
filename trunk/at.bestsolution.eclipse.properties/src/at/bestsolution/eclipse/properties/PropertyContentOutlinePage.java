b60273c0cda93eba628e366ded174ac1509a4453
tomsontom <tomsontom@737c282a-10f1-5f32-d127-9b87a3a82760>
2011-01-09 14:00:02 +0000

    * fixed delta calculation * added possibility to switch between hierarchical/flat representation

--- trunk/at.bestsolution.eclipse.properties/src/at/bestsolution/eclipse/properties/PropertyContentOutlinePage.java
+++ trunk/at.bestsolution.eclipse.properties/src/at/bestsolution/eclipse/properties/PropertyContentOutlinePage.java
@@ -37,13 +37,18 @@ import org.eclipse.ui.views.contentoutline.ContentOutlinePage;
 import org.osgi.service.prefs.BackingStoreException;
 
 public class PropertyContentOutlinePage extends ContentOutlinePage {
-	private List<Object> list = new ArrayList<Object>();
-	private List<Property> currentList = new ArrayList<Property>();
+	private List<Object> hierarchicalStructure = new ArrayList<Object>();
+	private List<Property> flatStructure = new ArrayList<Property>();
+	private Pair[] properties = new Pair[0];
 	private IEclipsePreferences preferences = new InstanceScope().getNode("propertiesoutline");
 
 	private static final String PREF_SORTED = "sorted";
+	private static final String PREF_HIERARCHICAL = "hierarchical";
 	
 	private Action sortAction;
+	private Action hierarchicalAction;
+	
+	private String groupRegexp = "_|\\.|/";
 	
 	public PropertyContentOutlinePage() {
 		sortAction = new Action("",IAction.AS_CHECK_BOX) {
@@ -58,24 +63,43 @@ public class PropertyContentOutlinePage extends ContentOutlinePage {
 			}
 		};
 		sortAction.setImageDescriptor(Activator.getDefault().getImageRegistry().getDescriptor(Activator.ALPHASORT_ICON));
-		sortAction.setChecked(preferences.getBoolean(PREF_SORTED, true));
+		sortAction.setChecked(isSorted());
+		
+		hierarchicalAction = new Action("",IAction.AS_CHECK_BOX) {
+			@Override
+			public void run() {
+				preferences.putBoolean(PREF_HIERARCHICAL, isChecked());
+				try {
+					preferences.flush();
+				} catch (BackingStoreException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		hierarchicalAction.setImageDescriptor(Activator.getDefault().getImageRegistry().getDescriptor(Activator.HIERACHICAL_ICON));
+		hierarchicalAction.setChecked(isHierarchical());
 		
 		preferences.addPreferenceChangeListener(new IPreferenceChangeListener() {
 			
 			public void preferenceChange(PreferenceChangeEvent event) {
 				if( event.getKey().equals(PREF_SORTED) ) {
-					boolean val = preferences.getBoolean(PREF_SORTED, true);
+					boolean val = isSorted();
 					setSorted(val);
 					sortAction.setChecked(val);
+				} else if( event.getKey().equals(PREF_HIERARCHICAL) ) {
+					boolean val = isHierarchical();
+					setHierarchical(val);
+					hierarchicalAction.setChecked(val);
 				}
 			}
 		});
 	}
-	
+
 	@Override
 	public void makeContributions(IMenuManager menuManager,
 			IToolBarManager toolBarManager, IStatusLineManager statusLineManager) {
 		super.makeContributions(menuManager, toolBarManager, statusLineManager);
+		toolBarManager.add(hierarchicalAction);
 		toolBarManager.add(sortAction);
 		
 		Action a = new Action("",IAction.AS_PUSH_BUTTON) {
@@ -97,7 +121,9 @@ public class PropertyContentOutlinePage extends ContentOutlinePage {
 					if( e1 instanceof PropertyGroup && e2 instanceof PropertyGroup ) {
 						return ((PropertyGroup)e1).name.compareTo(((PropertyGroup)e2).name);
 					} else if( e1 instanceof Property && e2 instanceof Property ) {
-						return ((Property)e1).pair.key.compareTo(((Property)e2).pair.key);
+						String k1 = ((Property)e1).pair.key.replaceAll(groupRegexp, "");
+						String k2 = ((Property)e2).pair.key.replaceAll(groupRegexp, "");
+						return k1.compareTo(k2);
 					} else if( e1 instanceof Property ) {
 						return -1;
 					} else if( e2 instanceof Property ) {
@@ -109,6 +135,82 @@ public class PropertyContentOutlinePage extends ContentOutlinePage {
 		} else {
 			getTreeViewer().setComparator(null);
 		}
+	}
+	
+	protected void setHierarchical(boolean val) {
+		//FIXME We should delay until visible!
+		if( val ) {
+			getTreeViewer().setInput(hierarchicalStructure);
+		} else {
+			getTreeViewer().setInput(flatStructure);
+		}
+	}
+	
+	private void createHierarchicalStructure() {
+		List<Property> found = new ArrayList<Property>();
+		List<Property> added = new ArrayList<Property>();
+		for (Pair property : properties) {
+			boolean isFound = false;
+			Iterator<Property> it = flatStructure.iterator();
+			while (it.hasNext()) {
+				Property p = it.next();
+				if (p.pair.key.equals(property.key)) {
+					p.pair=property;
+					isFound = true;
+					found.add(p);
+					it.remove();
+				}
+			}
+			if (!isFound) {
+				added.add(new Property(property));
+			}
+		}
+
+		// These are removed
+		for (Property p : flatStructure) {
+			if (p.parent != null) {
+				p.parent.items.remove(p);
+				p.parent = null;
+			} else {
+				hierarchicalStructure.remove(p);
+			}
+		}
+
+		flatStructure.clear();
+		flatStructure.addAll(found);
+
+		for (Property p : added) {
+			List<String> parts = new ArrayList<String>(Arrays.asList(p.pair.key
+					.split(groupRegexp)));
+			if (parts.size() > 1) {
+				PropertyGroup group = getGroup(hierarchicalStructure, parts);
+				if (parts.size() > 1) {
+					PropertyGroup tmp = group;
+					PropertyGroup root = null;
+					for (int i = 0; i < parts.size() - 1; i++) {
+						tmp = new PropertyGroup(tmp, parts.get(i));
+						if (i == 0) {
+							root = tmp;
+						}
+					}
+
+					p.parent = tmp;
+					p.parent.items.add(p);
+
+					if (group == null) {
+						hierarchicalStructure.add(root);
+					}
+				} else {
+					p.parent = group;
+					p.parent.items.add(p);
+				}
+			} else {
+				hierarchicalStructure.add(p);
+			}
+			flatStructure.add(p);
+		}
+
+		removeEmptyGroups(hierarchicalStructure);
 	}
 	
 	@Override
@@ -137,84 +239,38 @@ public class PropertyContentOutlinePage extends ContentOutlinePage {
 				super.update(cell);
 			}
 		});
-		viewer.setContentProvider(new ContentProvider());
-		if( preferences.getBoolean(PREF_SORTED, true) ) {
+		
+		viewer.setContentProvider(new HierarchicalContentProvider());
+				
+		if( isSorted() ) {
 			setSorted(true);
 		}
-		viewer.setInput(list);
+		
+		createHierarchicalStructure();
+		
+		if( isHierarchical() ) {
+			viewer.setInput(hierarchicalStructure);			
+		} else {
+			viewer.setInput(flatStructure);
+		}
 	}
 
 	public void setProperties(Pair[] properties) {
-		List<Property> found = new ArrayList<Property>();
-		List<Property> added = new ArrayList<Property>();
-		for (Pair property : properties) {
-			boolean isFound = false;
-			Iterator<Property> it = currentList.iterator();
-			while (it.hasNext()) {
-				Property p = it.next();
-				if (p.pair.key.equals(property)) {
-					isFound = true;
-					found.add(p);
-					it.remove();
-				}
-			}
-			if (!isFound) {
-				added.add(new Property(property));
-			}
-		}
-
-		// These are removed
-		for (Property p : currentList) {
-			if (p.parent != null) {
-				p.parent.items.remove(p);
-				p.parent = null;
-			} else {
-				list.remove(p);
-			}
-		}
-
-		currentList.clear();
-		currentList.addAll(found);
-
-		for (Property p : added) {
-			List<String> parts = new ArrayList<String>(Arrays.asList(p.pair.key
-					.split("_")));
-			if (parts.size() > 1) {
-				PropertyGroup group = getGroup(list, parts);
-				if (parts.size() > 1) {
-					PropertyGroup tmp = group;
-					PropertyGroup root = null;
-					for (int i = 0; i < parts.size() - 1; i++) {
-						tmp = new PropertyGroup(tmp, parts.get(i));
-						if (i == 0) {
-							root = tmp;
-						}
-					}
-
-					p.parent = tmp;
-					p.parent.items.add(p);
-
-					if (group == null) {
-						list.add(root);
-					}
-				} else {
-					p.parent = group;
-					p.parent.items.add(p);
-				}
-			} else {
-				list.add(p);
-			}
-			currentList.add(p);
-		}
-
-		removeEmptyGroups(list);
-
+		this.properties = properties;
+		createHierarchicalStructure();
 		if (getTreeViewer() != null) {
 			getTreeViewer().refresh();
 		}
-
 	}
 
+	private boolean isSorted() {
+		return preferences.getBoolean(PREF_SORTED, true);
+	}
+	
+	private boolean isHierarchical() {
+		return preferences.getBoolean(PREF_HIERARCHICAL, false);
+	}
+	
 	private void removeEmptyGroups(List<?> list) {
 		Iterator<?> it = list.iterator();
 		while (it.hasNext()) {
@@ -256,7 +312,7 @@ public class PropertyContentOutlinePage extends ContentOutlinePage {
 		}
 	}
 
-	static class ContentProvider implements ITreeContentProvider {
+	static class HierarchicalContentProvider implements ITreeContentProvider {
 
 		public void dispose() {
 
